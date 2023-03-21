e2c5f00bc982e0ad9a913291a3426b457bab5c8d
Titus Wormer <tituswormer@gmail.com>
2023-03-21 21:16:01 +0100

    fetch

---
+++ script/build.mjs
@@ -0,0 +1,52 @@
+import assert from 'node:assert/strict'
+import fs from 'node:fs/promises'
+import {fetch} from 'undici'
+
+const repo = 'wooorm/markdown-tm-language'
+const branch = 'main'
+const filename = 'source.mdx.tmLanguage'
+
+const branchesResponse = await fetch(
+  `https://api.github.com/repos/${repo}/branches`
+)
+
+const branches =
+  /** @type {Array<{name: string, commit: {sha: string, url: string}, protected: boolean}>} */ (
+    await branchesResponse.json()
+  )
+const main = branches.find((d) => d.name === branch)
+assert(main, 'expected `' + branch + '` branch')
+
+const sha = main.commit.sha
+
+const blobResponse = await fetch(
+  `https://raw.githubusercontent.com/${repo}/${branch}/${filename}`
+)
+let blob = await blobResponse.text()
+
+let injected = false
+
+blob = blob.replace(/<dict>/, ($0) => {
+  injected = true
+  return `<!--
+    This file is maintained at <https://github.com/${repo}/blob/${branch}/${filename}>.
+    To improve it, please create a pull request to the original repository.
+    Once accepted there, it can be pulled into this project (\`vscode-mdx\`) with
+    \`script/build.js\`, and released.
+
+    Version from SHA: ${sha}.
+  -->
+  ${$0}`
+})
+
+assert(injected, 'expected to find a dict')
+
+await fs.writeFile(
+  new URL(
+    '../packages/vscode-mdx/syntaxes/source.mdx.tmLanguage',
+    import.meta.url
+  ),
+  blob
+)
+
+console.log('Wrote `source.mdx.tmLanguage` at `' + sha + '`')

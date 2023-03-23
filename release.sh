1fee4efbcc74e2b80b0f23bbc7e36d2eafba0744
Remco Haszing <remcohaszing@gmail.com>
2023-03-23 11:01:27 +0100

    Remove npm publish from release.sh
    This will be added back later. We need to publish manually the the
    initial release.

--- release.sh
+++ release.sh
@@ -5,6 +5,5 @@ npm pack --pack-destination "$PWD" --workspace @mdx-js/language-monaco
 npx --workspace vscode-mdx vsce package --out "$PWD"
 
 npx changeset publish
-npm publish ./*.tgz
 npx ovsx publish --packagePath ./*.vsix
 npx vsce publish --packagePath ./*.vsix

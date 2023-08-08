d77942b81905dbb7ccd3e7ed5d777985f56f7a85
Remco Haszing <remcohaszing@gmail.com>
2023-08-08 16:23:29 +0200

    Use JavaScript build script (#335)
    VSCode related packages are improperly dual published. By resolving to
    ESM instead of CJS, we can slightly reduce bundle size.
    
    More importantly, the upcoming transition to Volar will add additional
    features, some of which rely on dependencies that don’t even work
    without a custom ESBuild plugin.

---
+++ packages/vscode-mdx/script/build.mjs
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+import {createRequire} from 'node:module'
+import process from 'node:process'
+import {fileURLToPath} from 'node:url'
+import {build} from 'esbuild'
+
+const require = createRequire(import.meta.url)
+
+const debug = process.argv.includes('debug')
+
+await build({
+  bundle: true,
+  entryPoints: {
+    extension: require.resolve('../src/extension.js'),
+    'language-server': require.resolve('@mdx-js/language-server')
+  },
+  external: ['vscode'],
+  logLevel: 'info',
+  minify: !debug,
+  outdir: fileURLToPath(new URL('../out/', import.meta.url)),
+  platform: 'node',
+  sourcemap: debug,
+  target: 'node16',
+  plugins: [
+    {
+      name: 'alias',
+      setup({onResolve, resolve}) {
+        onResolve(
+          {filter: /^(vscode-.*|jsonc-parser)$/},
+          ({path, ...options}) =>
+            resolve(require.resolve(path).replace(/\/umd\//, '/esm/'), options)
+        )
+        onResolve({filter: /\/umd\//}, ({path, ...options}) =>
+          resolve(path.replace(/\/umd\//, '/esm/'), options)
+        )
+      }
+    }
+  ]
+})

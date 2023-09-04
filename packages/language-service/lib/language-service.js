5cb6e820f60808bd12fa314cf5b976911eecfb9b
Remco Haszing <remcohaszing@gmail.com>
2023-09-04 08:47:30 +0200

    Use volar-service-markdown and volar-service-yaml

--- packages/language-service/lib/language-service.js
+++ packages/language-service/lib/language-service.js
@@ -3,10 +3,10 @@
  * @typedef {import('unified').PluggableList} PluggableList
  */
 
-import createTypeScriptService from 'volar-service-typescript'
+import {create as createMarkdownService} from 'volar-service-markdown'
+import {create as createTypeScriptService} from 'volar-service-typescript'
+import {create as createYamlService} from 'volar-service-yaml'
 import {getLanguageModule} from './language-module.js'
-import {createMarkdownService} from './volar-service-markdown/index.cjs'
-import {createYamlService} from './volar-service-yaml/index.js'
 
 /**
  * @param {Config} config
@@ -19,9 +19,9 @@ export function resolveConfig(config, ts, plugins) {
   config.languages.mdx ||= getLanguageModule(ts, plugins)
 
   config.services ||= {}
-  config.services.typescript = createTypeScriptService.default()
   config.services.markdown = createMarkdownService()
-  config.services.yaml = createYamlService()
+  config.services.typescript = createTypeScriptService()
+  config.services.yaml = createYamlService({})
 
   return config
 }

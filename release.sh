580eb9578e4de193354e4ea6d0216728ba5c78e6
Remco Haszing <remcohaszing@gmail.com>
2023-12-14 15:24:55 +0100

    Don’t specify type module in top level package.json
    This messes with the fixture tests.

--- release.sh
+++ release.sh
@@ -3,7 +3,7 @@ npm pack --pack-destination "$PWD" --workspace @mdx-js/language-service
 npm pack --pack-destination "$PWD" --workspace @mdx-js/language-server
 npx --workspace vscode-mdx vsce package --out "$PWD"
 
-node scripts/tag-extension.js
+node scripts/tag-extension.mjs
 npx changeset publish
 npx ovsx publish --packagePath ./*.vsix
 npx vsce publish --packagePath ./*.vsix

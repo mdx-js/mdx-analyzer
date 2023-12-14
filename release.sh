f7ac84b474cab8ea0d2535f59e16b74d6e82b92d
Remco Haszing <remcohaszing@gmail.com>
2023-12-14 17:37:32 +0100

    Fix GitHub releases of vscode-mdx (#369)
    The solution is based on
    https://github.com/statelyai/xstate-tools/blob/8a5df5db0e96e097b51e58280ae6405f2a0f9f94/scripts/tag-extension.js

--- release.sh
+++ release.sh
@@ -3,6 +3,7 @@ npm pack --pack-destination "$PWD" --workspace @mdx-js/language-service
 npm pack --pack-destination "$PWD" --workspace @mdx-js/language-server
 npx --workspace vscode-mdx vsce package --out "$PWD"
 
+node scripts/tag-extension.mjs
 npx changeset publish
 npx ovsx publish --packagePath ./*.vsix
 npx vsce publish --packagePath ./*.vsix

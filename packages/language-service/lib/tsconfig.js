a748c43285c6f8208f4d80cff2dc98405ac0ae65
Remco Haszing <remcohaszing@gmail.com>
2023-12-27 16:14:26 +0100

    Refactor loading remark plugins from tsconfig.json
    `@mdx-js/language-service` now exposes a function
    `resolveRemarkPlugins`, which resolves remark plugins in a environment
    agnostic manner.
    
    The language server implements Node.js specific logic to resolve this
    from `tsconfig.json`.

---
+++ packages/language-service/lib/tsconfig.js
@@ -0,0 +1,60 @@
+/**
+ * @typedef {import('typescript').ParsedCommandLine} ParsedCommandLine
+ * @typedef {import('unified').Pluggable} Pluggable
+ * @typedef {import('unified').PluggableList} PluggableList
+ * @typedef {import('unified').Plugin} Plugin
+ */
+
+/**
+ * Resolve remark plugins from TypeScript’s parsed command line options.
+ *
+ * @param {unknown} mdxConfig
+ *   The parsed command line options from which to resolve plugins.
+ * @param {(name: string) => Plugin | PromiseLike<Plugin>} resolvePlugin
+ *   A function to resolve a single remark plugin.
+ * @returns {Promise<PluggableList | undefined>}
+ *   An array of resolved plugins, or `undefined` in case of an invalid configuration.
+ */
+export async function resolveRemarkPlugins(mdxConfig, resolvePlugin) {
+  if (
+    typeof mdxConfig !== 'object' ||
+    !mdxConfig ||
+    !('plugins' in mdxConfig)
+  ) {
+    return
+  }
+
+  const pluginConfig = mdxConfig.plugins
+
+  if (typeof pluginConfig !== 'object' || !pluginConfig) {
+    return
+  }
+
+  const pluginArray = Array.isArray(pluginConfig)
+    ? pluginConfig
+    : Object.entries(pluginConfig)
+
+  if (pluginArray.length === 0) {
+    return
+  }
+
+  /** @type {Promise<Pluggable>[]} */
+  const plugins = []
+  for (const maybeTuple of pluginArray) {
+    const [name, ...options] = Array.isArray(maybeTuple)
+      ? maybeTuple
+      : [maybeTuple]
+
+    if (typeof name !== 'string') {
+      continue
+    }
+
+    plugins.push(
+      Promise.resolve(name)
+        .then(resolvePlugin)
+        .then((plugin) => [plugin, ...options])
+    )
+  }
+
+  return Promise.all(plugins)
+}

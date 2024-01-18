b9a910e4d9e87535e2973f2d7b5704d0489bc2e0
Remco Haszing <remcohaszing@gmail.com>
2024-01-18 14:09:07 +0100

    Create commands to toggle prose syntax (#384)
    The prose syntax is based on the current text selection and the AST.
    If the selection is within range of the syntax that’s toggled, the
    syntax is removed. Otherwise the selected text is expanded to include
    full words and wrapped.
    
    The supported syntaxes are:
    
    - `delete`
    - `emphasis`
    - `inlineCode`
    - `strong`
    
    Closes #353

---
+++ packages/language-service/lib/mdast-utils.js
@@ -0,0 +1,41 @@
+/**
+ * @typedef {import('mdast').Nodes} Nodes
+ * @typedef {import('unist').Point} Point
+ * @typedef {import('unist').Position} Position
+ */
+
+/**
+ * Get the offset of a parsed unist point.
+ *
+ * @param {Point} point
+ *   The unist point of which to get the offset.
+ * @returns {number}
+ *   The offset of the unist point.
+ */
+export function getPointOffset(point) {
+  return /** @type {number} */ (point.offset)
+}
+
+/**
+ * Get the start offset of a parsed unist point.
+ *
+ * @param {Nodes} node
+ *   The unist point of which to get the start offset.
+ * @returns {number}
+ *   The start offset of the unist point.
+ */
+export function getNodeStartOffset(node) {
+  return getPointOffset(/** @type {Position} */ (node.position).start)
+}
+
+/**
+ * Get the end offset of a parsed unist point.
+ *
+ * @param {Nodes} node
+ *   The unist point of which to get the end offset.
+ * @returns {number}
+ *   The end offset of the unist point.
+ */
+export function getNodeEndOffset(node) {
+  return getPointOffset(/** @type {Position} */ (node.position).end)
+}

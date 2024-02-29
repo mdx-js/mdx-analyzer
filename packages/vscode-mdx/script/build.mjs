4c60767bb3d0cddb581062d73fd03681c9160874
Remco Haszing <remcohaszing@gmail.com>
2024-02-29 13:55:39 +0100

    Simplify the build script

--- packages/vscode-mdx/script/build.mjs
+++ packages/vscode-mdx/script/build.mjs
@@ -8,40 +8,19 @@ const require = createRequire(import.meta.url)
 
 const debug = process.argv.includes('debug')
 
-build({
+await build({
   bundle: true,
   entryPoints: {
-    extension: require.resolve('../src/extension.js'),
-    'language-server': require.resolve('@mdx-js/language-server')
+    'out/extension': require.resolve('../src/extension.js'),
+    'out/language-server': require.resolve('@mdx-js/language-server'),
+    'node_modules/@mdx-js/typescript-plugin': require.resolve(
+      '../src/typescript-plugin.js'
+    )
   },
   external: ['vscode'],
   logLevel: 'info',
   minify: !debug,
-  outdir: fileURLToPath(new URL('../out/', import.meta.url)),
-  platform: 'node',
-  sourcemap: debug,
-  target: 'node16',
-  plugins: [
-    {
-      name: 'alias',
-      setup({onResolve, resolve}) {
-        onResolve({filter: /^(jsonc-parser)$/}, ({path, ...options}) =>
-          resolve(require.resolve(path).replace(/\/umd\//, '/esm/'), options)
-        )
-        onResolve({filter: /\/umd\//}, ({path, ...options}) =>
-          resolve(path.replace(/\/umd\//, '/esm/'), options)
-        )
-      }
-    }
-  ]
-})
-
-build({
-  bundle: true,
-  entryPoints: ['./src/typescript-plugin.js'],
-  outfile: './node_modules/typescript-plugin-bundled/index.js',
-  logLevel: 'info',
-  minify: !debug,
+  outdir: fileURLToPath(new URL('../', import.meta.url)),
   platform: 'node',
   sourcemap: debug,
   target: 'node16',

fa4d4287e4ae82559d2c1c179efaa4b394aa34cc
Remco Haszing <remcohaszing@gmail.com>
2024-03-01 20:15:31 +0100

    Fix type errors

--- packages/language-server/test/document-symbols.test.js
+++ packages/language-server/test/document-symbols.test.js
@@ -11,8 +11,8 @@ let serverHandle
 
 beforeEach(async () => {
   serverHandle = createServer()
-  // @ts-expect-error https://github.com/volarjs/volar.js/pull/142
   await serverHandle.initialize(fixtureUri('node16'), {
+    // @ts-expect-error https://github.com/volarjs/volar.js/pull/142
     typescript: {enabled: true, tsdk}
   })
 })
--- packages/language-server/test/no-tsconfig.test.js
+++ packages/language-server/test/no-tsconfig.test.js
@@ -10,8 +10,8 @@ let serverHandle
 
 beforeEach(async () => {
   serverHandle = createServer()
-  // @ts-expect-error https://github.com/volarjs/volar.js/pull/142
   await serverHandle.initialize(fixtureUri('no-tsconfig'), {
+    // @ts-expect-error https://github.com/volarjs/volar.js/pull/142
     typescript: {enabled: true, tsdk}
   })
 })

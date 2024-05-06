8cbb48bd9ffc9d2d6566b59c49a7694ec7240d0b
Remco Haszing <remcohaszing@gmail.com>
2024-05-06 16:01:49 +0200

    Add tests for resolveRemarkPlugins

---
+++ packages/language-service/test/tsconfig.js
@@ -0,0 +1,76 @@
+/**
+ * @typedef {import('unified').Plugin} Plugin
+ */
+
+import assert from 'node:assert/strict'
+import {test} from 'node:test'
+import {resolveRemarkPlugins} from '@mdx-js/language-service'
+
+test('ignore null', async () => {
+  const result = await resolveRemarkPlugins(null, () => () => {})
+
+  assert.equal(result, undefined)
+})
+
+test('ignore non-objects', async () => {
+  const result = await resolveRemarkPlugins('string', () => () => {})
+
+  assert.equal(result, undefined)
+})
+
+test('ignore objects without `plugins` key', async () => {
+  const result = await resolveRemarkPlugins({}, () => () => {})
+
+  assert.equal(result, undefined)
+})
+
+test('ignore null plugins', async () => {
+  const result = await resolveRemarkPlugins({plugins: null}, () => () => {})
+
+  assert.equal(result, undefined)
+})
+
+test('ignore non-object plugins', async () => {
+  const result = await resolveRemarkPlugins({plugins: 'string'}, () => () => {})
+
+  assert.equal(result, undefined)
+})
+
+test('ignore empty plugins', async () => {
+  const result = await resolveRemarkPlugins({plugins: []}, () => () => {})
+
+  assert.equal(result, undefined)
+})
+
+test('load array of plugin tuples', async () => {
+  /** @type {Record<string, Plugin>} */
+  const plugins = {
+    a() {},
+    b() {}
+  }
+
+  const result = await resolveRemarkPlugins(
+    {plugins: ['a', ['b', 'b options'], 42]},
+    (name) => plugins[name]
+  )
+
+  assert.deepEqual(result, [[plugins.a], [plugins.b, 'b options']])
+})
+
+test('load object plugin mappings', async () => {
+  /** @type {Record<string, Plugin>} */
+  const plugins = {
+    a() {},
+    b() {}
+  }
+
+  const result = await resolveRemarkPlugins(
+    {plugins: {a: undefined, b: 'b options'}},
+    (name) => plugins[name]
+  )
+
+  assert.deepEqual(result, [
+    [plugins.a, undefined],
+    [plugins.b, 'b options']
+  ])
+})

8240ab1f764c699ff31648b0dd5407c7436a84ac
Remco Haszing <remcohaszing@gmail.com>
2024-09-19 17:27:34 +0200

    Use multiple tsconfigs for language-service
    Since only the lib files of the language service need type emit, this is
    split into two tsconfigs. For a more consistent layout, the other
    packages now also have all their source code inside the lib folders.

--- packages/language-service/index.js
+++
@@ -1,3 +0,0 @@
-export {createMdxLanguagePlugin} from './lib/language-plugin.js'
-export {createMdxServicePlugin} from './lib/service-plugin.js'
-export {resolveRemarkPlugins} from './lib/tsconfig.js'
--- packages/vscode-mdx/script/build.mjs
+++ packages/vscode-mdx/script/build.mjs
@@ -14,7 +14,7 @@ await build({
     'out/extension': require.resolve('../src/extension.js'),
     'out/language-server': require.resolve('@mdx-js/language-server'),
     'node_modules/@mdx-js/typescript-plugin': require.resolve(
-      '../../typescript-plugin/index.cjs'
+      '../../typescript-plugin/lib/index.cjs'
     )
   },
   external: ['vscode'],

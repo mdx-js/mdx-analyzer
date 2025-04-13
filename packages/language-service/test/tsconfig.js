fa0a6cfcdf109925068731e7d2fbd30adde75d44
Remco Haszing <remcohaszing@gmail.com>
2025-04-13 17:09:30 +0200

    Make the resolveRemarkPlugins tests synchronous

--- packages/language-service/test/tsconfig.js
+++ packages/language-service/test/tsconfig.js
@@ -6,50 +6,50 @@ import assert from 'node:assert/strict'
 import {test} from 'node:test'
 import {resolveRemarkPlugins} from '@mdx-js/language-service'
 
-test('ignore null', async () => {
-  const result = await resolveRemarkPlugins(null, () => () => {})
+test('ignore null', () => {
+  const result = resolveRemarkPlugins(null, () => () => {})
 
   assert.equal(result, undefined)
 })
 
-test('ignore non-objects', async () => {
-  const result = await resolveRemarkPlugins('string', () => () => {})
+test('ignore non-objects', () => {
+  const result = resolveRemarkPlugins('string', () => () => {})
 
   assert.equal(result, undefined)
 })
 
-test('ignore objects without `plugins` key', async () => {
-  const result = await resolveRemarkPlugins({}, () => () => {})
+test('ignore objects without `plugins` key', () => {
+  const result = resolveRemarkPlugins({}, () => () => {})
 
   assert.equal(result, undefined)
 })
 
-test('ignore null plugins', async () => {
-  const result = await resolveRemarkPlugins({plugins: null}, () => () => {})
+test('ignore null plugins', () => {
+  const result = resolveRemarkPlugins({plugins: null}, () => () => {})
 
   assert.equal(result, undefined)
 })
 
-test('ignore non-object plugins', async () => {
-  const result = await resolveRemarkPlugins({plugins: 'string'}, () => () => {})
+test('ignore non-object plugins', () => {
+  const result = resolveRemarkPlugins({plugins: 'string'}, () => () => {})
 
   assert.equal(result, undefined)
 })
 
-test('ignore empty plugins', async () => {
-  const result = await resolveRemarkPlugins({plugins: []}, () => () => {})
+test('ignore empty plugins', () => {
+  const result = resolveRemarkPlugins({plugins: []}, () => () => {})
 
   assert.equal(result, undefined)
 })
 
-test('load array of plugin tuples', async () => {
+test('load array of plugin tuples', () => {
   /** @type {Record<string, Plugin>} */
   const plugins = {
     a() {},
     b() {}
   }
 
-  const result = await resolveRemarkPlugins(
+  const result = resolveRemarkPlugins(
     {plugins: ['a', ['b', 'b options'], 42]},
     (name) => plugins[name]
   )
@@ -57,14 +57,14 @@ test('load array of plugin tuples', async () => {
   assert.deepEqual(result, [[plugins.a], [plugins.b, 'b options']])
 })
 
-test('load object plugin mappings', async () => {
+test('load object plugin mappings', () => {
   /** @type {Record<string, Plugin>} */
   const plugins = {
     a() {},
     b() {}
   }
 
-  const result = await resolveRemarkPlugins(
+  const result = resolveRemarkPlugins(
     {plugins: {a: undefined, b: 'b options'}},
     (name) => plugins[name]
   )
